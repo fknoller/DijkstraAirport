c454dd7a390a0ca9cb17bbb95ad04f5721574583
Felipe Knöller <66882589+fknoller@users.noreply.github.com>
2022-09-10 18:53:54 -0300

    Add Connect.java for mySQL connection

---
+++ src/server/Connect.java
@@ -0,0 +1,25 @@
+package server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private String server, db, user, password;
+    private int port;
+
+    public Connect(int status) {
+        if(status == 0) {
+            this.server = "127.0.0.1";
+            this.db = "airport";
+            this.user = "root";
+            this.password = "pass123";
+            this.port = 3306;
+        }
+    }
+
+    public Connection connect() throws SQLException {
+        String url = "jdbc:mysql://" + this.server + ":" + this.port + "/" + this.db;
+        return DriverManager.getConnection(url, this.user, this.password);
+    }
+}
